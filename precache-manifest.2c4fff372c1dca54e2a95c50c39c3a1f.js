bb04f8dfd4b5db997832f6bdd9a1a062ecb9ac5b
nutchapon <nutchapon.han@gmail.com>
2019-11-22 14:00:36 +0700

    Updates

---
+++ precache-manifest.2c4fff372c1dca54e2a95c50c39c3a1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e5854307f3f3b35ca95b059ccb3f8e",
+    "url": "https//caption01.github.io/monsterReact/index.html"
+  },
+  {
+    "revision": "c26907a1a7c11bec7e6d",
+    "url": "https//caption01.github.io/monsterReact/static/css/main.0061e15b.chunk.css"
+  },
+  {
+    "revision": "65227b0674bf51fa8248",
+    "url": "https//caption01.github.io/monsterReact/static/js/2.840b9e8c.chunk.js"
+  },
+  {
+    "revision": "c26907a1a7c11bec7e6d",
+    "url": "https//caption01.github.io/monsterReact/static/js/main.2a06ddb9.chunk.js"
+  },
+  {
+    "revision": "d13062387256f9c18105",
+    "url": "https//caption01.github.io/monsterReact/static/js/runtime-main.319a0cd8.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https//caption01.github.io/monsterReact/precache-manifest.2c4fff372c1dca54e2a95c50c39c3a1f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https//caption01.github.io/monsterReact/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
